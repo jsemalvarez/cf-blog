f6ca1fc197804b52a01e40202fc03d21f5b7e032
jsemalvarez <jsemalvarez@gmail.com>
2024-02-01 23:30:05 -0300

    feat: nest g res admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { AdminModule } from './admin/admin.module';
 
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule, PostsModule, AdminModule],
 })
 export class AppModule {}
