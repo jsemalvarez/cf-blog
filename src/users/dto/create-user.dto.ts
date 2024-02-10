c9f597402cb7ef506833e038089d55d41ab17b79
jsemalvarez <jsemalvarez@gmail.com>
2024-02-10 16:58:29 -0300

    feat: user creation validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,19 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
 
+    @IsString()
+    @MinLength(3)
     name: string;
 
+    @IsString()
+    @MinLength(3)
     user: string;
 
+    @IsEmail()
     email: string;
 
+    @MinLength(8)
+    @IsString()
     password: string;
 }
