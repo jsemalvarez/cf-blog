056f8e17e49d055f1b2576d299bc9d5b620833ae
jsemalvarez <jsemalvarez@gmail.com>
2024-02-11 21:53:24 -0300

    feat: post dto validations

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,6 +1,12 @@
+import { IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreatePostDto {
 
+    @IsString()
+    @MinLength(3)
     title: string;
 
+    @IsString()
+    @MinLength(5)
     desciption: string;
 }
